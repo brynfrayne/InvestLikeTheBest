9c1517e1f679f086007e92b0a95352de9fc79fde
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-01-26 10:01:45 -0800

    added cik identifier to each holdings migration file table name

--- server/db/migrations/20220126174605_holdings_0001079114.js
+++ server/db/migrations/20220126174605_holdings_0001079114.js
@@ -1,15 +1,14 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('holdings_0001079114', (table) => {
+        table.string('name', 255).notNullable();
+        table.string('cusip',255).notNullable();
+        table.integer('value').notNullable();
+        table.integer('shares').notNullable();
+    })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('holdings');
 };
