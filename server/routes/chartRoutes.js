6804831e2d52ee766a0045a6451fbb92551e7b67
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-01-28 14:43:08 -0800

    created an endpoint sql query to receive all stocks listed by the top value

--- server/routes/chartRoutes.js
+++ server/routes/chartRoutes.js
@@ -4,8 +4,9 @@ const connection = require('../connection');
 
 
 
-router.get('/', function (_req, res) {
-    connection.query('select * from 13f_table', function (error, result, _fields) {
+router.get('/topStocks', function (_req, res) {
+    connection.query('select cusip,name,sum(value) as stockvalue from aggregate_holdings where period_of_report = "Q3-21" group by cusip', 
+    function (error, result, _fields) { 
        if (error) throw error;
        res.send((result));
      });
