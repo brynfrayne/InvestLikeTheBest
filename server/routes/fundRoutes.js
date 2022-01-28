f1dd543fabd080ea870d473026f3913e51360cb2
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-01-27 21:25:01 -0800

    added endpoint to fundroutes for specific fund

--- server/routes/fundRoutes.js
+++ server/routes/fundRoutes.js
@@ -3,4 +3,14 @@ const { json } = require("express/lib/response");
 const router = express.Router();
 const fs = require("fs");
 
+// get specific fund info -- 
+router.get('/:CIK', function (_req, res) {
+    // const CIK = req.params.CIK;
+    connection.query(`select * from aggregate_holdings where CIK='0000783412'`, function (error, result, _fields) {
+       if (error) throw error;
+       res.send((result));
+     });
+ });
+
+
 module.exports = router;
