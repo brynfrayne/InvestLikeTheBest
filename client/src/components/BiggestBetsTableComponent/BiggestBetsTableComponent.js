4d02ee1a5fb29f29cbcf7052933d193c62aa58e9
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-02-02 20:19:46 -0800

    added links to the biggest bets page

--- client/src/components/BiggestBetsTableComponent/BiggestBetsTableComponent.js
+++ client/src/components/BiggestBetsTableComponent/BiggestBetsTableComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Table } from 'react-bootstrap';
 import uniqid from 'uniqid';
+import { Link } from 'react-router-dom';
 
 export default function BiggestBetsTableComponent({fund}) {
 let sortedFund= fund.sort((a,b)=> ((b.value/b.portfolioValue) - (a.value/a.portfolioValue)));
@@ -17,8 +18,8 @@ let sortedFund= fund.sort((a,b)=> ((b.value/b.portfolioValue) - (a.value/a.portf
           
             {sortedFund.map((holding) => ( 
                <tr className="company-ownership-table__row" key={uniqid()}  >
-                  <td className="company-ownership-table__row-item" >{holding.investor   }</td>
-                  <td className="company-ownership-table__row-item" >{holding.name}</td>
+                  <td className="company-ownership-table__row-item" ><Link  className='table-links' to={`/funds/${holding.CIK}/Q3-21`}>{holding.investor}</Link></td>
+                  <td className="company-ownership-table__row-item" ><Link className='table-links' to={`/company/${holding.cusip}/${holding.period_of_report}`}>{holding.name}</Link></td>
                   <td className="company-ownership-table__row-item" >{Math.round((holding.value/holding.portfolioValue)*100)+'%'}</td> 
                 </tr>
              ))
