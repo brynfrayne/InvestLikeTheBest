1c8091ed0b504de3e2027b0d63222d4a3369a688
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-02-06 15:28:12 -0800

    moved wall street bets endpoint from index.js to company routes page

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
-const knex = require('knex')
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const cors = require("cors");
 const port = process.env.port || 8000;
-const companyRoutes = require('./routes/companyRoutes');
-const fundRoutes = require('./routes/fundRoutes');
-const chartRoutes = require('./routes/chartRoutes');
 const { config } = require('dotenv');
 const fetch = require('node-fetch');
 require("dotenv").config();
 
+const companyRoutes = require('./routes/companyRoutes');
+const fundRoutes = require('./routes/fundRoutes');
+const chartRoutes = require('./routes/chartRoutes');
 
 const connection = mysql.createConnection({
   host: '127.0.0.1',
@@ -30,8 +29,12 @@ app.use(express.json());
 // app.use(express.urlencoded());
 app.use(cors());
 
+//  ROUTES
+app.use("/funds", fundRoutes);
+app.use("/company", companyRoutes);
+app.use("/charts", chartRoutes);
 
-//rest api to get all holdings
+// rest api to get all holdings
 app.get('/filings', function (_req, res) {
    connection.query('select * from aggregate_holdings', function (error, result, _fields) {
 	  if (error) throw error;
@@ -39,13 +42,6 @@ app.get('/filings', function (_req, res) {
 	});
 });
 
-// fetches reddit wall street bets data
-app.get('/reddit', async (req, res) =>  {
-const api_url = 'https://tradestie.com/api/v1/apps/reddit';
-const fetch_response = await fetch(api_url);
-const json = await fetch_response.json();
-res.send(json);
-} )
 
   //  this endpoint is to fetch the ticker symbol
  app.get('/:cusip', async (req, res) => {
@@ -59,7 +55,7 @@ res.send(json);
 // this end point will fetch the logo for the ticker
 app.get('/:cusip/:ticker', async (req, res) => {
   const ticker = req.params.ticker;
-  console.log(ticker)
+  
   const api_url = `https://api.twelvedata.com/logo?symbol=${ticker}&apikey=${process.env.REACT_APP_twelveData_apiKey}`;
   const fetch_response = await fetch(api_url);
   const json = await fetch_response.json();
@@ -67,10 +63,7 @@ app.get('/:cusip/:ticker', async (req, res) => {
  }); 
 
 
-//  ROUTES
-app.use("/funds", fundRoutes);
-app.use("/company", companyRoutes);
-app.use("/charts", chartRoutes);
+
 
 
 app.listen(port, () => {
--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -1,16 +1,25 @@
-const { default: axios } = require("axios");
 const express = require("express");
 const router = express.Router();
 const connection = require('../connection');
 const fetch = require('node-fetch');
 const cors = require("cors");
+const { config } = require('dotenv');
 
 require("dotenv").config();
 
 //  MIDDLEWARE
-router.use(express.json());
+// router.use(express.json());
 // router.use(express.urlencoded());
 router.use(cors());
+
+// fetches reddit wall street bets data
+router.get('/reddit', async (req, res) =>  {
+  const api_url = 'https://tradestie.com/api/v1/apps/reddit';
+  const fetch_response = await fetch(api_url);
+  const json = await fetch_response.json();
+  res.json(json);
+  } )
+
 
 router.get('/:period_of_report', function (req, res) {
   const period_of_report = req.params.period_of_report;
@@ -21,29 +30,20 @@ router.get('/:period_of_report', function (req, res) {
        res.send((result));
      });
  });
-
-
- 
  
 
-  router.get('/:cusip/:period_of_report', function (req, res) {
-    const cusip = req.params.cusip;
-    const period_of_report = req.params.period_of_report;
-    connection.query(`select * from aggregate_holdings where (period_of_report = "${period_of_report}" and cusip = "${cusip}" )order by cusip`, 
-    function (error, result, _fields) { 
-       if (error) throw error;
-       res.send((result));
-     });
+router.get('/:cusip/:period_of_report', function (req, res) {
+  const cusip = req.params.cusip;
+  const period_of_report = req.params.period_of_report;
+  connection.query(`select * from aggregate_holdings where (period_of_report = "${period_of_report}" and cusip = "${cusip}" )order by cusip`, 
+  function (error, result, _fields) { 
+      if (error) throw error;
+      res.send((result));
+    });
  });
 
 
-// for whatever reason this endpoint below only works in index.js ??
-router.get('/reddit', async (req, res) =>  {
-  const api_url = 'https://tradestie.com/api/v1/routers/reddit';
-  const fetch_response = await fetch(api_url);
-  const json = await fetch_response.json();
-  res.send(json);
-  } )
+
 
 module.exports = router;
  
