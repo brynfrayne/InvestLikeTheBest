5080805dfb0fb9506b2bdb2ed3a20b969eafe3ac
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-02-07 11:57:14 -0800

    commiting small changes before leaving branch

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -23,7 +23,7 @@ router.get('/reddit', async (req, res) =>  {
 
 router.get('/:period_of_report', function (req, res) {
   const period_of_report = req.params.period_of_report;
-    connection.query(`select cusip,name,period_of_report,shares,count(distinct fund) as stockCount from aggregate_holdings where period_of_report = "${period_of_report}" group by cusip`, 
+    connection.query(`select cusip, name, period_of_report, shares, count(distinct fund) as stockCount from aggregate_holdings where period_of_report = "${period_of_report}" group by cusip`, 
 
     function (error, result, _fields) { 
        if (error) throw error;
