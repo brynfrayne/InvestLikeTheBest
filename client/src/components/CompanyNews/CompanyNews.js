8c6d0604a563621dec47fc4e98998d8dd2938b7f
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-02-08 22:00:25 -0800

    added some basic styling to news article containers

--- client/src/components/CompanyNews/CompanyNews.js
+++ client/src/components/CompanyNews/CompanyNews.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './CompanyNews.scss';
+import uniqid from 'uniqid';
+
 
 export default class CompanyNews extends Component {
 state = {
@@ -13,21 +15,26 @@ state = {
             .then(response => {
                 console.log(response.data)
                 this.setState({
-                    news:response.data
+                    news:response.data.slice(0,20)
                 })
             })
     }
   render() {
+      console.log(this.state.news)
       if (!this.state.news) {
         return <div class="loader"></div>
       }
     return <div>
-        <Link to={this.state.news[0].url}>
-            <div className='news__image' style={{backgroundImage: `url(${this.state.news[0].image})`}}></div>
-            <h5>{this.state.news[0].title}</h5>
-            <p>{this.state.news[0].text}</p>
-            <p>{this.state.news[0].date}</p>
-        </Link>
+        {this.state.news.map(item=> {
+        return <section className='news'><Link className='news__link' to={{pathname: item.url}} target="_blank" key={uniqid()}>
+            <h5 className='news__title'>{item.title}</h5>
+            <div className='news__date-site-container'>
+                <p className='news__date'>{item.publishedDate.split(' ')[0]}</p>
+                <p className='news__site'>{item.site}</p>
+            </div>
+            <p className='news__text'>{item.text}</p>
+        </Link></section>
+        })}
     </div>;
   }
 }
