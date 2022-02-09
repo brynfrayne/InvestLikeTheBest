0ca468ef39bdb4c9f8bf8ea65cca4db5f51b557b
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-02-09 14:26:48 -0800

    added responsive styling to news articles

--- client/src/components/SpecificCompanyHeader/SpecificCompanyHeader.js
+++ client/src/components/SpecificCompanyHeader/SpecificCompanyHeader.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './SpecificCompanyHeader.scss';
 export default class SpecificCompanyHeader extends Component {
   state = {
     price: null,
@@ -10,7 +11,6 @@ export default class SpecificCompanyHeader extends Component {
   componentDidMount() {
     axios.get(`http://localhost:8000/company/${this.props.companyData.ticker}/price`)
           .then(response => {
-            console.log(response.data)
             this.setState({
               price:response.data[0]
             })
@@ -29,12 +29,18 @@ export default class SpecificCompanyHeader extends Component {
     }
     
   return <div>
-      <h1 className="company-page__title">{this.props.companyData.name}</h1>
-      <img className='company-page__image' src={this.state.img}  alt='/'/>
-      <p className="company-page__ticker">Ticker: {this.props.companyData.ticker}</p>
-      <p className="company-page__ticker">Price: $ {this.state.price.price}</p>
-      <p className="company-page__ticker">Volume:{this.state.price.volume}</p>
-      
+      <h1 className="company-header__title">{this.props.companyData.name}</h1>
+      <img className='company-header__image' src={this.state.img}  alt='/'/>
+      <div className='company-header__ticker-price-volume-container'>
+        <div className='company-header__ticker-exchange-box'>
+          <p className="company-header__ticker">Ticker: {this.props.companyData.ticker}</p>
+          <p className="company-header__ticker">Primary Exchange: {this.props.companyData.primary_exchange}</p>
+        </div>
+        <div className='company-header__price-volume-box'>
+          <p className="company-header__ticker">Price: $ {this.state.price.price}</p>
+          <p className="company-header__ticker">Volume:{this.state.price.volume}</p>
+        </div>
+      </div>
     </div>;
   }
 }
